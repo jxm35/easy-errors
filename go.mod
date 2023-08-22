55e8e34a0e0cd4858d0bf17eb68d4878199bf198
sc20jjb <sc20jjb@leeds.ac.uk>
2023-08-22 17:12:12 +0100

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jxm35/easy-errors
+
+go 1.19
---
+++ result/result.go
@@ -0,0 +1,60 @@
+package result
+
+type Result[T any] interface {
+	GetError() error
+	Get() (T, error)
+	Value() T
+}
+
+type resultImpl[T any] struct {
+	val T
+	err error
+}
+
+func (r *resultImpl[T]) GetError() error {
+	return r.err
+}
+func (r *resultImpl[T]) Get() (T, error) {
+	return r.val, r.err
+}
+func (r *resultImpl[T]) Value() T {
+	return r.val
+}
+
+func Ok[T any](val T) Result[T] {
+	return &resultImpl[T]{
+		val: val,
+		err: nil,
+	}
+}
+func Err[T any](err error) Result[T] {
+	return &resultImpl[T]{
+		err: err,
+	}
+}
+
+func Map[A, B any](resA Result[A], f func(A) B) Result[B] {
+	if err := resA.GetError(); err != nil {
+		return Err[B](err)
+	}
+	v := f(resA.Value())
+	return Ok(v)
+}
+
+func MapErr[A, B any](resA Result[A], f func(A) (B, error)) Result[B] {
+	if err := resA.GetError(); err != nil {
+		return Err[B](err)
+	}
+	v, err := f(resA.Value())
+	if err != nil {
+		return Err[B](err)
+	}
+	return Ok(v)
+}
+
+func Bind[A, B any](resA Result[A], f func(A) Result[B]) Result[B] {
+	if err := resA.GetError(); err != nil {
+		return Err[B](err)
+	}
+	return f(resA.Value())
+}
